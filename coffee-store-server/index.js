964b915d7419764d97c65c4f1da4c79576613ea1
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-11-30 12:22:44 +0600

    Server Side Hide Database User and Password Using Dotenv Config

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,12 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMaster
-// BacgJTxvsAg7T9FC
-
-
-// const uri = "mongodb+srv://coffeeMaster:<db_password>@cluster0.dmztt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://coffeeMaster:BacgJTxvsAg7T9FC@cluster0.dmztt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmztt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +28,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
